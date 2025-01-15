d1227c69b195776ab347b162e77815b3350bd00d
MentalPlus <contact@mentalplus.fr>
2025-01-15 20:58:08 +0100

    Changement du nom de MentalPlus vers MentalSerenity

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const config = {
-    siteName: 'MentalPlus',
+    siteName: 'MentalSerenity',
     siteDescription: 'Votre plateforme de bien-être mental',
     contact: {
-        email: 'contact@mentalplus.fr',
+        email: 'contact@mentalserenity.fr',
         phone: 'votre_numero',
         address: 'votre_adresse'
     },
     social: {
-        facebook: 'https://facebook.com/mentalplus',
-        instagram: 'https://instagram.com/mentalplus',
-        linkedin: 'https://linkedin.com/company/mentalplus'
+        facebook: 'https://facebook.com/mentalserenity',
+        instagram: 'https://instagram.com/mentalserenity',
+        linkedin: 'https://linkedin.com/company/mentalserenity'
     },
     meta: {
-        title: 'MentalPlus - Votre bien-être mental',
-        description: 'MentalPlus vous accompagne dans votre parcours de bien-être mental avec des professionnels qualifiés.',
+        title: 'MentalSerenity - Votre bien-être mental',
+        description: 'MentalSerenity vous accompagne dans votre parcours de bien-être mental avec des professionnels qualifiés.',
         keywords: 'bien-être mental, psychologie, thérapie, consultation, santé mentale'
     }
 };
